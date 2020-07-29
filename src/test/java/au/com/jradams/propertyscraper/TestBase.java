644f3c66e04fd2d75ee1812deb993360b881f4e8
Justin <justinadams7991@gmail.com>
2020-07-29 22:09:41 +1000

    feat(config): Setup YAML config and store in model
    - add additional Gradle directories to .gitignore
    - add jackson dataformat, lombok, and commons-lang to build
    - add yaml model class to hold configuration
    - add yaml properties file to hold config data
    - test the model class within a basic test case

--- src/test/java/au/com/jradams/propertyscraper/TestBase.java
+++ src/test/java/au/com/jradams/propertyscraper/TestBase.java
@@ -1,6 +1,10 @@
 package au.com.jradams.propertyscraper;
 
+import au.com.jradams.propertyscraper.model.ConfigModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,18 +12,56 @@ import org.junit.runners.JUnit4;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(JUnit4.class)
 public class TestBase {
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //  Static Fields
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     protected static WebDriver driver;
 
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //  Instance Fields
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    private ConfigModel config;
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //  Pre-Test Method(s)
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     @BeforeClass
-    public static void setUp() {
+    public static void setupSuite() {
         System.setProperty("webdriver.chrome.driver", "/home/justin/projects/assets/property_scraper/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
     }
+
+    @Before
+    public void setupTest() {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+        try {
+            URL configFile = Thread.currentThread().getContextClassLoader().getResource("main.yaml");
+            ConfigModel config;
+
+            if (configFile != null) {
+                config = mapper.readValue(new File(configFile.getPath()), ConfigModel.class);
+
+                System.out.println(config);
+            }
+        } catch (IOException ioe) {
+            System.err.println("Exception occurred while reading configuration file. " + ioe);
+        }
+    }
+
+
 
     @AfterClass
     public static void tearDown() {
@@ -29,12 +71,11 @@ public class TestBase {
 
     @Test
     public void crawlDomainDotComAu() {
-
+        driver.get("https://www.domain.com.au");
     }
 
     @Test
     public void crawlRealEstateDotComAu() {
-        driver.get("https://www.domain.com.au");
     }
 
     @Test
